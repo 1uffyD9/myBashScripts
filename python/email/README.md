9d16aef6e777ade6c8980efa04fb64d0e8ad3afc
1uffyD9 <mugiwara.luffyd9@gmail.com>
2022-09-09 09:10:14 +0530

    Move configs to env file

--- python/email/README.md
+++ python/email/README.md
@@ -1,7 +1,22 @@
 
 
 ## Configuration
+Create a `.env` file inside the same directory and add the following content to it.
+
 ```bash
-SENDER_EMAIL="<email_here>"
-APP_PASS="<pass_here>"
+SERVER="<server_ip_or_domain"
+PORT=465
+SENDER_EMAIL="<sender_email_here>"
+APP_PASS="<sender_pass_here>"
+RECVR_EMAIL="<receiver_email_here>"
+```
+
+### Example
+
+```bash
+SERVER="smtp.gmail.com"
+PORT=465
+SENDER_EMAIL="sender_email@gmail.com"
+APP_PASS="this_is_not_the_password"
+RECVR_EMAIL="receiver_email@gmail.com"
 ```
--- python/email/send_mail.py
+++ python/email/send_mail.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 
-import os
-import smtplib, ssl
-from dotenv import load_dotenv
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import smtplib
+import ssl
+import os
 
 
 SCRIPT_ROOT = os.path.dirname(os.path.realpath(__file__))
 load_dotenv(f"{SCRIPT_ROOT}/.env")
+SERVER = os.getenv('SERVER')
+PORT = os.getenv('PORT')
 SENDER_EMAIL = os.getenv('SENDER_EMAIL')
 APP_PASS = os.getenv('APP_PASS')
-
+RECVR_EMAIL = os.getenv('RECVR_EMAIL')
 
 # ref : https://realpython.com/python-send-email/#sending-fancy-emails
 
-receiver_email = "test@example.com"
-
-message = MIMEMultipart("alternative")
-message["Subject"] = "multipart test"
-message["From"] = SENDER_EMAIL
-message["To"] = receiver_email
+message = MIMEMultipart('alternative')
+message['From'] = SENDER_EMAIL
+message['To'] = RECVR_EMAIL
+message['Subject'] = "This Mail sent using a python script"
 
 # Create the plain-text and HTML version of your message
-text = "Plain text content here"
+text = "This will be getting override by html content"
+html = ''
+html_template = 'template.html'
 
-html = ""
-
-with open("template.html", 'r') as template:
+# read from html file
+with open(html_template, 'r') as template:
     html = template.read()
 
 # Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
+part1 = MIMEText(text, 'plain') # this will be getting override by the next content
+part2 = MIMEText(html, 'html')
 
 # Add HTML/plain-text parts to MIMEMultipart message
 # The email client will try to render the last part first
@@ -41,9 +43,8 @@ message.attach(part2)
 
 # Create secure connection with server and send email
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+with smtplib.SMTP_SSL(SERVER, PORT, context=context) as server:
     server.login(SENDER_EMAIL, APP_PASS)
     server.sendmail(
-        SENDER_EMAIL, receiver_email, message.as_string()
+        SENDER_EMAIL, RECVR_EMAIL, message.as_string()
     )
-
